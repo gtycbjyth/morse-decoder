d8d63b56b281b5ddfd652971ea350f19621168a7
gtycbjyth <kovaltru@gmail.com>
2020-10-13 13:38:05 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -36,9 +36,38 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
+console.log(typeof MORSE_TABLE);
 function decode(expr) {
     // write your solution here
+    let goo = "";
+    for (let i = 10 ; i <= expr.length; i = i + 10) {
+        let j = i-10;
+        let str = expr.substring(j,i);
+        let word = "";
+        // console.log(str);
+        if (str === "**********"){
+            goo += " ";
+            continue ;
+        }
+        for (let d = 2; d <= 10; d=d + 2){
+            let c = d - 2;
+            let letter = str.substring(c,d);
+            // console.log(letter);
+            if (letter === "10") {
+                word += ".";
+            } if (letter === "11"){
+                word += "-";                
+            }
+            if (d > 9){
+                goo += MORSE_TABLE[word];
+                // console.log('word' + word);
+                // console.log(MORSE_TABLE[word]);
+            }
+        }
+        
+    }
+    // console.log(goo);
+    return goo;
 }
 
 module.exports = {
